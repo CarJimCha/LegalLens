27a8c10d85572fa6ccaafda56f5f144cda29a0ef
CJC <carlosjimcha@gmail.com>
2026-05-05 20:00:54 +0200

    Simple login added

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -124,3 +124,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Login Redirect
+
+LOGIN_REDIRECT_URL = '/'  # Te lleva al dashboard después de loguearte
+LOGOUT_REDIRECT_URL = '/accounts/login/' # Te devuelve al login al salir
--- backend/core/views.py
+++ backend/core/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import ContratoAuditado
+from django.contrib.auth.decorators import login_required
 
 def dashboard(request):
     contratos = ContratoAuditado.objects.all().order_by('-fecha_subida')
     return render(request, 'dashboard.html', {'contratos': contratos})
+
+
+@login_required
+def detalle_auditoria(request, pk):
+    # Buscamos el contrato o devolvemos 404 si no existe
+    contrato = get_object_or_404(ContratoAuditado, pk=pk)
+
+    # Extraemos el JSON
+    datos = contrato.resultado_json or {}
+
+    return render(request, 'core/detalle.html', {
+        'contrato': contrato,
+        'puntos_clave': datos.get('puntos_clave', []),
+        'banderas_rojas': datos.get('banderas_rojas', []),
+        'riesgo': contrato.riesgo
+    })
